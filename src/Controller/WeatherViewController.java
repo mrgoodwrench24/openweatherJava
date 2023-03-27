4afd4ff62f14367ddc6db20aed8c2bf0abe9f278
Michael White <michaeltroywhite24@gmail.com>
2023-03-26 22:32:46 -0600

    3.26.23.2

--- src/Controller/WeatherViewController.java
+++ src/Controller/WeatherViewController.java
@@ -1,16 +1,21 @@
 package Controller;
 
-import Model.OpenWeatherMapApi;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 public class WeatherViewController implements Initializable {
 
+    private HashMap cityInfo;
+
+    private HashMap currentConditions;
     @FXML
     private Label cityLabel;
 
@@ -32,10 +37,29 @@ public class WeatherViewController implements Initializable {
     @FXML
     private TextField zipCodeTextBox;
 
+    @FXML
+    void onActionSearchButton(ActionEvent event) throws IOException {
+        String zipCode = zipCodeTextBox.getText();
+        cityInfo = Model.OpenWeatherMapApi.getCityInfo(zipCode);
+        cityLabel.setText("Current conditions for " + cityInfo.get("name"));
+        String lat = (String) cityInfo.get("lat");
+        String lon = (String) cityInfo.get("lon");
+
+        currentConditions = Model.OpenWeatherMapApi.getCurrentConditions(lat,lon);
+        currentConditionsLabel.setText((String) currentConditions.get("description"));
+        currentTempLabel.setText(String.format("%.1f", currentConditions.get("temp")) + "F");
+        feelsLikeTempLabel.setText(String.format("%.1f", currentConditions.get("feels_like")) + "F");
+        highTempLabel.setText(String.format("%.1f", currentConditions.get("temp_max")) + "F");
+        lowTempLabel.setText(String.format("%.1f", currentConditions.get("temp_min")) + "F");
+
+
+
+
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        String apiKey = OpenWeatherMapApi.getApiKey();
-        System.out.println(apiKey);
 
     }
 }
--- src/Model/OpenWeatherMapApi.java
+++ src/Model/OpenWeatherMapApi.java
@@ -1,14 +1,22 @@
 package Model;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class OpenWeatherMapApi {
 
     private static String apiKey;
+    private static String city;
 
-    public static String getApiKey(){
+    private static String getApiKey(){
         File keyReader = new File("src/Model/apiKey.txt");
         try {
             Scanner myReader;
@@ -20,5 +28,82 @@ public class OpenWeatherMapApi {
             e.printStackTrace();
         }
         return apiKey;
+    }
+
+    public static HashMap getCityInfo(String zipCode) throws IOException {
+        getApiKey();
+        HashMap<String, String> cityInfo = new HashMap<>();
+        try {
+            URL url = new URL("http://api.openweathermap.org/geo/1.0/zip?zip=" + zipCode + "%2CUS&appid=" + apiKey);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+
+            BufferedReader in = new BufferedReader(new BufferedReader(new InputStreamReader(connection.getInputStream())));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) !=null){
+                response.append(inputLine);
+
+            }
+            in.close();
+
+            //Parse the JSON response and put the values into the HashMap
+            JSONObject jsonObject = new JSONObject(response.toString());
+            cityInfo.put("zip", jsonObject.getString("zip"));
+            cityInfo.put("name", jsonObject.getString("name"));
+            cityInfo.put("lat", Double.toString(jsonObject.getDouble("lat")));
+            cityInfo.put("lon", Double.toString(jsonObject.getDouble("lon")));
+
+        } catch (MalformedURLException e){
+            System.out.println("Bad URL");
+            e.printStackTrace();
+        }
+        return cityInfo;
+    }
+
+    public static HashMap getCurrentConditions(String lat, String lon){
+        HashMap<String, Object> weatherInfo = new HashMap<>();
+        try{
+            URL url = new URL("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new BufferedReader(new InputStreamReader(connection.getInputStream())));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) !=null){
+                response.append(inputLine);
+
+            }
+            in.close();
+
+            JSONObject jsonObject = new JSONObject(response.toString());
+
+            JSONObject mainObj = jsonObject.getJSONObject("main");
+            double tempKelvin = mainObj.getDouble("temp");
+            double feelK = mainObj.getDouble("feels_like");
+            double minK = mainObj.getDouble("temp_min");
+            double maxK = mainObj.getDouble("temp_max");
+            weatherInfo.put("temp", kelvinToFarenheit(tempKelvin));
+            weatherInfo.put("feels_like", kelvinToFarenheit(feelK));
+            weatherInfo.put("temp_min", kelvinToFarenheit(minK));
+            weatherInfo.put("temp_max", kelvinToFarenheit(maxK));
+
+            JSONArray weatherArray = jsonObject.getJSONArray("weather");
+            JSONObject weather = weatherArray.getJSONObject(0);
+            weatherInfo.put("description", weather.getString("description"));
+        } catch (ProtocolException e) {
+            throw new RuntimeException(e);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return weatherInfo;
+    }
+
+    private static double kelvinToFarenheit(double temp){
+        return ((temp - 273.15) * 9/5 +32);
     }
 }
